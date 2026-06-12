fb8991bea45900e7ee1107688177bbd7034149fb
Erick Francisco Castillo Badillo <franky431216@hotmail.com>
2026-06-12 12:14:38 -0600

    Replace Kafka with RabbitMQ

--- src/main/java/com/mobica/catalog/CatalogServiceApplication.java
+++ src/main/java/com/mobica/catalog/CatalogServiceApplication.java
@@ -2,9 +2,9 @@ package com.mobica.catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.EnableKafka;
 
-@EnableKafka
+
+
 @SpringBootApplication
 public class CatalogServiceApplication {
 
