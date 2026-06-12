4e1c9687d04769233c17f4fd1dc280b83812cf0d
Erick Francisco Castillo Badillo <franky431216@hotmail.com>
2026-06-12 11:09:42 -0600

    Fix Kafka env vars 3

--- src/main/java/com/mobica/catalog/camel/PurchaseRoute.java
+++ src/main/java/com/mobica/catalog/camel/PurchaseRoute.java
@@ -9,20 +9,19 @@ public class PurchaseRoute extends RouteBuilder {
     @Override
     public void configure() {
 
-        //from("kafka:cart-events")
-            // .log("CAMEL RECIBIÓ -> ${body}")
+        from("kafka:cart-events")
+                .log("CAMEL RECIBIÓ -> ${body}")
 
-            // .setHeader("To", constant("franky431216@hotmail.com"))
-           //  .setHeader("Subject", constant("Producto agregado al carrito"))
+                .setHeader("To", constant("franky431216@hotmail.com"))
+                .setHeader("Subject", constant("Producto agregado al carrito"))
 
-            // .setBody(simple(
-              //   "Se recibió un evento:\n\n${body}"
-          //   ))
+                .setBody(simple(
+                        "Se recibió un evento:\n\n${body}"))
 
-           //  .to("smtp://{{smtp.host}}:{{smtp.port}}"
-               //  + "?username={{smtp.username}}"
-               //  + "&password={{smtp.password}}"
-             //    + "&mail.smtp.auth=true"
-           //      + "&mail.smtp.starttls.enable=true");
+                .to("smtp://{{smtp.host}}:{{smtp.port}}"
+                        + "?username={{smtp.username}}"
+                        + "&password={{smtp.password}}"
+                        + "&mail.smtp.auth=true"
+                        + "&mail.smtp.starttls.enable=true");
     }
 }
